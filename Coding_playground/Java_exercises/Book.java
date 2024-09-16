ffd0bfee0201b90b099c77b808a92d57f5e05f4b
Moa <m.l.elofsson@gmail.com>
2024-09-16 13:45:02 +0200

    I think I did it

--- Coding_playground/Java_exercises/Book.java
+++ Coding_playground/Java_exercises/Book.java
@@ -1,5 +1,28 @@
 package Coding_playground.Java_exercises;
 
 public class Book {
+
+    String title;
+    String author;
+    int year;
+
+    Book(String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+
+    }
+
+    public static void main(String[] args){
+        Book drivingMissDaisy = new Book("Driving Miss Daisy", "Brilliant Person", 1956);
+            
+
+        System.out.println(drivingMissDaisy);
+        System.out.println(drivingMissDaisy.title);
+        System.out.println(drivingMissDaisy.author);
+        System.out.println(drivingMissDaisy.year);
+
+    }
+
     
 }
