df35d1a1c7f10a3aa0bb43542a43d9d2dbe2c331
Moa <m.l.elofsson@gmail.com>
2024-09-20 11:18:59 +0200

    Finished one exercise, onto the next

--- Coding_playground/Java_exercises/Student.java
+++ Coding_playground/Java_exercises/Student.java
@@ -2,4 +2,23 @@ package Coding_playground.Java_exercises;
 
 public class Student {
     
+    String name;
+    int age;
+    double grade;
+
+    Student(String name, int age, double grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    
+    public static void main(String[] args) {
+
+        Student helmer = new Student("Helmer", 17, 4.2);
+
+        System.out.println(helmer.name);
+        System.out.println(helmer.age);
+        System.out.println(helmer.grade);
+        System.out.println();
+    }
 }
