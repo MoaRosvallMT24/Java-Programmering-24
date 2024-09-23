475b4f224a4a3eadb46bc67d08bcfdb91df5bcbc
Moa <m.l.elofsson@gmail.com>
2024-09-23 14:27:43 +0200

    OMW.

---
+++ Coding_playground/Java_labs/Die.java
@@ -0,0 +1,16 @@
+package Coding_playground.Java_labs;
+
+public class Die {
+
+    private int sides;
+    private int value;
+    
+    public Die(int sides){
+        this.sides = sides;
+    }
+
+    public void setValue(){
+
+    }
+        
+}
