eed055feef85610f0a85ee1e536c7aad843eff85
Moa <m.l.elofsson@gmail.com>
2024-09-23 14:21:25 +0200

    Working on method keywords

---
+++ Coding_playground/Java_exercises/MainMethodKeywords.java
@@ -0,0 +1,12 @@
+package Coding_playground.Java_exercises;
+
+public class MainMethodKeywords {
+    //main method måste vara public? jao
+    //main method måste vara static? njae
+    //void till int, fattar inte. 
+    //arguments working fine
+    public static void main(String[] args) {
+        System.out.print("main is working fine!");
+    }
+    
+}
