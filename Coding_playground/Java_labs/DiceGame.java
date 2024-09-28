d82369da334a80a7b6c7e93a1ca9d324c9645139
Moa <m.l.elofsson@gmail.com>
2024-09-28 15:01:15 +0200

    Working on scanner usage

--- Coding_playground/Java_labs/DiceGame.java
+++ Coding_playground/Java_labs/DiceGame.java
@@ -1,10 +1,28 @@
 package Coding_playground.Java_labs;
 
+import java.util.Scanner;
+
 public class DiceGame {
-    //scanner
-    //mainmetod
-    //fråga efter: spelomgångar, sides
-    //fråga efter spelarens namn
+    public Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to THE DICEGAME!");
+        System.out.println("How many rounds would you like to play?");
+        
+        int rounds = sc.nextInt();
+
+        System.out.println("How many sides to your die?");
+
+        int sides = sc.nextInt();
+
+        System.out.println("Last question! What should I call you?");
+
+        String userName = sc.nextLine();
+
+        System.out.println("Okay, " + userName + "! Let's play THE DICEGAME!");
+    }
+    
     
 
     //spelaren gissar värdet
--- Coding_playground/Java_labs/Die.java
+++ Coding_playground/Java_labs/Die.java
@@ -1,14 +1,27 @@
 package Coding_playground.Java_labs;
-import java.util.random.*;
+import java.util.Random;
 public class Die {
 
     private int sides;
     private int value;
     private Random rand;
     
-    public Die(int sides){
+    public Die(int getSides){
         this.sides = sides;
         this.value = 1;
+    }
+
+    public int getSides(){
+
+    }
+
+    public int getValue(){
+
+    }
+
+    public void roll(){
+        
+
     }
         
 }
