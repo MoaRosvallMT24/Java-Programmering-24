f68596e6de17fd418e6ca71c340bd1749543c094
Moa <m.l.elofsson@gmail.com>
2024-10-13 23:10:43 +0200

    Dicegame player

--- Coding_playground/Java_labs/Player.java
+++ Coding_playground/Java_labs/Player.java
@@ -1,22 +1,41 @@
+package Coding_playground.Java_labs;
+
 public class Player {
-  String name;
-  int score;
-  Die die;
+  private String name;
+  private int score;
+  private Die playerDie;
   
-  String getName(){
-    
+  public Player(String name){
+    this.name=name;
   }
-  
-    //namnets gettermetod
-    //poängens gettermetod
-    //poängens settermetod
 
-  //constructor(namn)
+  public String getName(){
+    return name;
+  }
 
-  //metoder för Player:
-    //rollDice - rullar tärningen
-    //getDieValue
-    //increaseScore
-    //addDie
+  public int getScore(){
+    return score;
+  }
 
+  public void setScore(int score){
+    this.score=score;
+  }
+
+  public void rollDice() {
+    playerDie.roll();
+  }
+
+  public int getDieValue(){
+    return playerDie.getValue(); 
+  }
+
+  public void increaseScore(){
+    score++;    
+  }
+
+  public void addDie(int playerSides){
+    Die playerNewDie = new Die(playerSides);
+    this.playerDie=playerNewDie;
+  }
+ 
 }
