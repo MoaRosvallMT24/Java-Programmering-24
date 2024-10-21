6c803d04379a2199adb37bb801381cd89c08acb6
Moa <m.l.elofsson@gmail.com>
2024-10-21 11:35:40 +0200

    Doing mathy stuff

---
+++ Coding_playground/Java_exercises/mathMain.java
@@ -0,0 +1,15 @@
+package Coding_playground.Java_exercises;
+
+public class mathMain {
+    public static void main(String[] args) {
+        
+        int input = 5;
+       
+        calculateFactorial(input);
+
+        System.out.println(factorial);
+
+
+    }
+    
+}
